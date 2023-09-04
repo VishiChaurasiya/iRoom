bed70bcbbfacf824f6cafe5111fb9546e370116c
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2023-09-04 20:03:28 +0530

    Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,25 +2,32 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Airbnb",
-    description: "Airbnb clone",
+  title: "Airbnb",
+  description: "Airbnb clone",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body className={font.className}>
-                <Navbar />
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
 }
