b65312ed096fd48662a07bad222f5cf673d563ee
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2023-12-18 21:04:02 +0530

    Reservations Screen

--- app/api/reservations/[reservationId]/route.ts
+++ app/api/reservations/[reservationId]/route.ts
@@ -23,7 +23,7 @@ export async function DELETE(
     throw new Error('Invalid ID');
   }
 
-  const reservation = await prisma.reservation.deleteMany({
+  const reservation = await prisma.reservation.delete({
     where: {
       id: reservationId,
       OR: [
